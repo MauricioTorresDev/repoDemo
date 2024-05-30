fe99db1364b67c35e627de42000510771e9123b4
MauricioTorresDev <mauricio.torres.b@vallegrande.edu.pe>
2024-05-30 15:52:11 -0500

    demo2

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const morgan = require('morgan')
 const path = require('path')
 const PORT = 3000;
 const ip = 'localhost'
-const connection = require("./db/db")
+
 
 app.use(morgan('dev'))
 app.use(express.static('public/'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 app.get('/atencion', (req, res) => {
